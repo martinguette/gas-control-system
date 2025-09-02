1ed5e8f53a62669663482b1fb729daaf07a38c21
martin guette <zerisawa33@gmail.com>
2025-09-01 23:51:42 -0500

    add route "log-in" and fix env variable

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import LoginForm from "@/components/auth/login-form"
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return <LoginForm />
+  redirect('/log-in');
 }
